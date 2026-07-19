1144d8c99f3e3e8a0f6a11d8f84978b9cbe71772
Lonnie Hutchinson <chinacat@chinacat.org>
2026-07-18 17:27:27 -0700

    make fixture a class so fixture.kwargs works with static type checkers

--- src/fixtures/_core.py
+++ src/fixtures/_core.py
@@ -74,18 +74,75 @@ class _KwargSubstitution:
 # Type annotations are intentionally missing since python type annotations do
 # not support adding kwargs and it is not possible to properly type this
 # functionality.
-def fixture(  # type: ignore[no-untyped-def]
-    factory,
-    *fixture_args,
-    fixture_name=None,
-    **fixture_kwargs,
-):
+class fixture:
+    """
+    fixture is a decorator that is used to call functions and inject the return
+    value as a keyword argument into the call to the decorate function. It can
+    be stacked to create many objects to pass to the decorated function. This
+    is useful for creating fixtures for tests.
+    While this can all be done in the test function itself this allows the
+    setup for the test to be clearly separated from the test action and
+    validation. This separation makes it easier to see what the initial state
+    of the test is and makes it easier to reuse test setup.
+    While 'explicit is better than implicit', many of the transient variables
+    to hold fixtures are eliminated from the code through good naming (TODO
+    add an example).
 
-    def decorator(func):  # type: ignore[no-untyped-def]
+    ###
+    # data model (also the fixture factories)
+    ###
+    @dataclass
+    class Point:
+        x: int
+        y: int
+
+    @dataclass
+    class Circle:
+        center: Point
+        radius: float
+
+    ###
+    # Tests
+    ###
+    @fixture(Point, 0, 0, fixture_name='center')
+    @fixture(Circle, radius=3, fixture_name='outer')
+    @fixture(Circle, radius=2, fixture_name='inner')
+    def test_case(outer, inner, **_):
+        ...
+    """
+
+    kwargs = _KwargSubstitution()
+    """
+    fixture.kwargs['fixture_name'] can be used as a fixture_kwarg to use a
+    keyword argument in the fixture stack as a different keyword argument to
+    a fixture factory function. This is useful when fixture factories take
+    arguments created by other fixtures when the names of the fixture and
+    argument don't match. For example:
+        @fixture(factory1, fixture_name='foo')
+        @fixture(factory2, bar=fixture.kwargs['foo'])
+        ...
+    The first @fixture will create a fixture in the keyword args named foo. The
+    second @fixture will call factory2 with a keyword argument bar with a value
+    that is the fixture named foo created by the first @fixture.
+    """
+
+    def __init__(  # type: ignore[no-untyped-def]
+        self,
+        factory,
+        *fixture_args,
+        fixture_name=None,
+        **fixture_kwargs,
+    ):
+        self.factory = factory
+        self.fixture_args = fixture_args
+        self.fixture_name = fixture_name or get_default_fixture_name(factory)
+        self.fixture_kwargs = fixture_kwargs
+
+    def __call__(self, func):  # type: ignore[no-untyped-def]
         @wraps(func)
         def wrapper(*args, **kwargs):  # type: ignore[no-untyped-def]
             substituted_fixture_kwargs = _KwargSubstitution._substitute_kwargs(
-                fixture_kwargs, kwargs
+                self.fixture_kwargs, kwargs
             )
 
             # The factory is not passed args because those are the args to the
@@ -93,19 +150,13 @@ def fixture(  # type: ignore[no-untyped-def]
             # However, kwargs are provided as they will contain the fixtures
             # that should be available to the fixture function so fixtures
             # can be stacked.
-            _fixture = factory(
-                *fixture_args,
+            _fixture = self.factory(
+                *self.fixture_args,
                 **kwargs,
                 **substituted_fixture_kwargs,
             )
 
-            name = fixture_name or get_default_fixture_name(factory)
-            kwargs[name] = _fixture
+            kwargs[self.fixture_name] = _fixture
             return func(*args, **kwargs)
 
         return wrapper
-
-    return decorator
-
-
-fixture.kwargs = _KwargSubstitution()  # type: ignore[attr-defined]
